21a56a357b4963321099f478b4e46836782ff89a
Jose Barraza <jose.barraza@couchsurfing.com>
2026-01-23 18:42:21 -0600

    fix e2e

--- apps/frontend/cypress/e2e/home.cy.ts
+++ apps/frontend/cypress/e2e/home.cy.ts
@@ -8,6 +8,6 @@ describe('SmartAgenda E2E', () => {
   it('should display the dashboard content', () => {
     cy.visit('/dashboard')
     cy.get('h2').contains('Dashboard')
-    cy.contains('Welcome to your new dashboard').should('be.visible')
+    cy.contains('Welcome Back').should('be.visible')
   })
 })
